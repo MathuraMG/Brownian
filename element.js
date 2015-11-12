63bb2f01fe5cdcec68241e17ce0c4773fc8e2027
MathuraMG <mmg9291@gmail.com>
2015-11-12 00:31:07 -0500

    added smoothing between brownian curves

--- element.js
+++ element.js
@@ -3,7 +3,7 @@ function elt() {
   this.ay = [];
   this.init = function() {
     this.ax[0] =-80; //width/2;
-    this.ay[0] = height / 2+ random(-range, range);
+    this.ay[0] = height / 2+ random(-range/1.2, range/1.2);
   }
   this.drawLine = function() {
   num = this.ax.length;
@@ -13,8 +13,8 @@ function elt() {
     lastXVal = this.ax[(num) - 1];
     lastYVal = this.ay[(num) - 1];
     // Put a new value at the end of the array
-    this.ax.push(lastXVal + random(0, range));
-    this.ay.push(lastYVal + random(-range, range));
+    this.ax.push(lastXVal + random(0, range*1.2));
+    this.ay.push(lastYVal + random(-range/1.2, range/1.2));
     num = this.ax.length;
     // Constrain all points to the screen
     this.ax[num - 1] = constrain(this.ax[num - 1], 0, width);
--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@
  */
 
 var num = 100;
-var range = 50;
+var range = 10;
 
 var ax = [];
 var ay = [];
@@ -18,7 +18,7 @@ var ay = [];
 //   ax: [],
 //   ay: []
 // };
-var lines = [];
+var lines1, lines2, lines3, lines4; // = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -27,18 +27,45 @@ function setup() {
 
   //frameRate(30);
   l = new elt();
-  lines = l;
+  lines1 = l;
   l.init();
-  strokeWeight(0.1);
-  stroke(0, 0, 0, 50);
+  strokeWeight(0.5);
+  stroke(random(200, 255), random(20, 75), random(20, 75), 50);
   l.drawLine();
+  l = new elt();
+  lines2 = l;
+  l.init();
+
+  l.drawLine();
+  curvesBW(lines1, lines2);
+  l = new elt();
+  lines3 = l;
+  l.init();
+
+  l.drawLine();
+  curvesBW(lines2, lines3);
+  l = new elt();
+  lines4 = l;
+  l.init();
+
+  l.drawLine();
+  curvesBW(lines4, lines3);
+  // l = new elt();
+  // lines4 = l;
+  // l.init();
+  // strokeWeight(0.5);
+  // stroke(0, 0, 0, 50);
+  // l.drawLine();
+  //curvesBW(lines3,lines1);
+  //curvesBW(lines4,lines1);
 
 }
 
 function draw() {
 
   //randomNoise();
-  curves();
+  //curves();
+
 
 
 }
@@ -50,11 +77,13 @@ function curves() {
       l = new elt();
       l.init();
       for (var a = 1; a < lines.ax.length; a++) {
-        l.ax[a] = lines.ax[a] + random(-2, 2);
-        l.ay[a] = lines.ay[a] + random(-2, 2);
+        l.ax[a] = lines.ax[a] + random(-3, 3);
+        l.ay[a] = lines.ay[a] + random(-3, 3);
         strokeWeight(0.1);
         stroke(random(200, 255), random(20, 75), random(20, 75), 50);
+        noFill();
         line(l.ax[a], l.ay[a], l.ax[a - 1], l.ay[a - 1]);
+        //arc(l.ax[a], l.ay[a], dist(l.ax[a - 1], l.ay[a - 1],l.ax[a], l.ay[a]), random(5,10),random(0,0.5), random(0.5,1));
       }
       lines = l;
 
@@ -67,6 +96,27 @@ function curves() {
   }
 }
 
+function curvesBW(lines1, lines2) {
+  for (var i = 0; i < 50; i++) {
+    l = new elt();
+    l.init();
+    for (var a = 1; a < lines2.ax.length; a++) {
+
+      l.ax[a] = lines1.ax[a] + ((lines2.ax[a] - lines1.ax[a]) / 50) * i;
+      l.ay[a] = lines1.ay[a] + ((lines2.ay[a] - lines1.ay[a]) / 50) * i;
+      strokeWeight(0.5);
+      stroke(random(200, 255), random(20, 75), random(20, 75), 50);
+      noFill();
+      line(l.ax[a], l.ay[a], l.ax[a - 1], l.ay[a - 1]);
+      //arc(l.ax[a], l.ay[a], dist(l.ax[a - 1], l.ay[a - 1],l.ax[a], l.ay[a]), random(5,10),random(0,0.5), random(0.5,1));
+    }
+    //lines = l;
+
+  }
+
+
+}
+
 function randomNoise() {
   if (frameCount < 1000) {
     //background(51);
@@ -74,7 +124,7 @@ function randomNoise() {
       l = new elt();
       lines = l;
       l.init();
-      strokeWeight(0.1);
+      strokeWeight(0.5);
       stroke(random(200, 255), random(20, 75), random(20, 75), 50);
       l.drawLine();
 
